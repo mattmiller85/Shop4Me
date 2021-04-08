fddcb14d45a0803182525153153c6fc0bc95b542
Matt Miller <matt.miller@centricconsulting.com>
2021-04-07 20:33:20 -0400

    change config back

--- web/src/app/config.ts
+++ web/src/app/config.ts
@@ -10,8 +10,8 @@ export default () => {
     apis: [
       {
         name: 'api',
-        endpoint: 'http://localhost:3000/dev',
-        // endpoint: 'https://xj3ib1h8w0.execute-api.us-east-1.amazonaws.com/dev',
+        // endpoint: 'http://localhost:3000/dev',
+        endpoint: 'https://xj3ib1h8w0.execute-api.us-east-1.amazonaws.com/dev',
         region: 'us-east-2',
         custom_header: async () => {
           return { Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}` };
